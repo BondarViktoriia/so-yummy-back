6d0974f20d3df68d85d905dcac6af0361342ac56
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-04-01 01:41:56 +0300

    model, getShoppingList cntrl

---
+++ models/shoppingList.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const IngredientSchema = Schema({
+  ingredientName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+});
+
+const joiVerifyIngredient = Joi.object({
+  ingredientName: Joi.string().required(),
+  quantity: Joi.string().required(),
+});
+
+const shoppingListSchema = Schema({
+  owner: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [IngredientSchema],
+    default: undefined,
+  },
+});
+
+const ShoppingList = model("shoppingList", shoppingListSchema);
+
+module.exports = {
+  ShoppingList,
+  joiVerifyIngredient,
+};
