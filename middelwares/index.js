0090740d7392d82b1422573cb124d0807cc663c9
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-03-31 23:06:04 +0300

    add upload,auth,and user model

--- middelwares/index.js
+++ middelwares/index.js
@@ -1,8 +1,10 @@
 const ctrlWrapper = require('./ctrlWrapper');
 const validation = require('./validation');
+const auth = require('./auth')
 
 
 module.exports = {
     ctrlWrapper,
-    validation
+    validation,
+    auth
 }
