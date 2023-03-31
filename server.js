5a4ff15bc9c39ec0f1879dd3f306462a93e14df7
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-03-31 13:05:43 +0300

    test

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 const app = require('./app')
 
-// const { DB_HOST, PORT = 3000 } = process.env;
-const DB_HOST ="mongodb+srv://viktoriiashvetsinteravia:group7@cluster0.qest4en.mongodb.net/db-soYummy?retryWrites=true&w=majority"
+const { DB_HOST, PORT = 3000 } = process.env;
 
 
 mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
-.then(()=>app.listen(3000, () => {
+.then(()=>app.listen(PORT, () => {
   console.log("Server running. Use our API on port: 3000")
 }))
   .catch(error => {
