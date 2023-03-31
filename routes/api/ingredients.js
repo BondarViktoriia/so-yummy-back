55b233cdc1816aec13acdb7f997932a468982c9f
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2023-03-31 22:47:25 +0300

    added request of all ingred

--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
+const Ingredient = require('../../models/ingredient');
 
+const ctrl = {};
 
+ctrl.ingredients = async (req, res) => {
+  const allIngreds = await Ingredient.find();
 
+  res.status(201).json(allIngreds);
+};
 
+router.get('/', ctrl.ingredients);
 
-module.exports = router
+module.exports = router;
