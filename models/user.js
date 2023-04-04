c079685c5faf10ca7d7567efa83a9ceb79ab343c
Comondear <elcomondear@gmail.com>
2023-04-04 13:30:23 -0600

    add-name

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ const Joi = require("joi");
 
 const userSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
     password: {
       type: String,
       required: [true, "Password is required"],
