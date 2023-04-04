50fbc72ddc696bd5c21abe28ec2867a2904c07e4
lady-araminta <jparkhomenko@gmail.com>
2023-04-04 19:05:24 +0300

    upd favorites

--- controllers/recipe/getFavorite.js
+++ controllers/recipe/getFavorite.js
@@ -2,12 +2,18 @@ const { RequestError } = require("../../helpers");
 const { Recipe } = require("../../models");
 
 const getFavorite = async (req, res) => {
-  const { _id } = req.user;
-  const data = await Recipe.find({ favorites: _id });
-  if (!data) {
+  const { _id: userId } = req.user;
+  const result = await Recipe.find({ favorites: userId });
+  if (!result) {
     throw RequestError(404, "Not found");
   }
-  res.status(200).json(data);
+  res.status(200).json({
+    code: 200,
+    status: "success",
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = { getFavorite };
