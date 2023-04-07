dbfe695ec07a79beb1186e71c8274a9424bb2fc5
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-04-07 18:48:15 +0300

    recipe + ingreds

--- controllers/recipe/getRecipeById.js
+++ controllers/recipe/getRecipeById.js
@@ -1,15 +1,49 @@
+const { mongoose } = require("mongoose");
 const { Recipe } = require("../../models");
 const { RequestError } = require("../../helpers");
 
 const getRecipeById = async (req, res) => {
   const { id } = req.params;
-  const recepie = await Recipe.findById(id);
+  const ObjectId = mongoose.Types.ObjectId;
 
-  if (!recepie) throw RequestError(404, "Not found");
+  const recipe = await Recipe.aggregate([
+    { $match: { _id: ObjectId(`${id}`) } },
+    {
+      $lookup: {
+        from: "ingreds",
+        localField: "ingredients.id",
+        foreignField: "_id",
+        as: "ingredientsInfo",
+      },
+    },
+    {
+      $set: {
+        ingredients: {
+          $map: {
+            input: "$ingredients",
+            in: {
+              $mergeObjects: [
+                "$$this",
+                {
+                  $arrayElemAt: [
+                    "$ingredientsInfo",
+                    { $indexOfArray: ["$ingredientsInfo._id", "$$this.id"] },
+                  ],
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+    { $unset: ["ingredientsInfo", "ingredients.id"] },
+  ]);
+
+  if (!recipe) throw RequestError(404, "Not found");
 
   res.status(200).json({
     data: {
-      recepie,
+      recipe,
     },
   });
 };
