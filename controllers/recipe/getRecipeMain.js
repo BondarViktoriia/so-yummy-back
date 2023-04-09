caaf9938c833f6f4086754dd2c9823f86c405b87
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-09 14:40:44 +0300

    corection

--- controllers/recipe/getRecipeMain.js
+++ controllers/recipe/getRecipeMain.js
@@ -4,25 +4,24 @@ const getRecipeMain = async (req, res) => {
   
     const result = []
   const chicken = await Recipe.find({
-   id:1,
         category: ["Chicken"] ,        
     }, '-createdAt -updatedAt').limit(4)
     result.push(chicken)
 
   const breakfast = await Recipe.find({
-      id:2,
+
       category: ["Breakfast"],
     }, '-createdAt -updatedAt').limit(4)
     result.push(breakfast)
 
   const desserts = await Recipe.find({
-      id:3,
+
       category: ["Dessert"],
     }, '-createdAt -updatedAt').limit(4)
      result.push(desserts)
     
   const miscellaneous = await Recipe.find({
-      id:4,
+
       category: ["Miscellaneous"],
     }, '-createdAt -updatedAt').limit(4)
     
