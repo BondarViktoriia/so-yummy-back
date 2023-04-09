a6458490be2d5c899d63e02f6b4d36a491d35924
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-09 12:37:35 +0300

    add

--- controllers/recipe/getRecipeMain.js
+++ controllers/recipe/getRecipeMain.js
@@ -3,22 +3,26 @@ const {Recipe} = require('../../models')
 const getRecipeMain = async (req, res) => {
   
     const result = []
-    const chicken = await Recipe.find({
+  const chicken = await Recipe.find({
+   id:1,
         category: ["Chicken"] ,        
     }, '-createdAt -updatedAt').limit(4)
     result.push(chicken)
 
-    const breakfast = await Recipe.find({
+  const breakfast = await Recipe.find({
+      id:2,
       category: ["Breakfast"],
     }, '-createdAt -updatedAt').limit(4)
     result.push(breakfast)
 
-    const desserts = await Recipe.find({
+  const desserts = await Recipe.find({
+      id:3,
       category: ["Dessert"],
     }, '-createdAt -updatedAt').limit(4)
      result.push(desserts)
     
-    const miscellaneous = await Recipe.find({
+  const miscellaneous = await Recipe.find({
+      id:4,
       category: ["Miscellaneous"],
     }, '-createdAt -updatedAt').limit(4)
     
