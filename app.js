682409822a49c733e10c413b8bf3f8f6e0ed8574
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-16 23:03:41 +0300

    fix app err

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
-
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message})
+})
 
 module.exports = app;
